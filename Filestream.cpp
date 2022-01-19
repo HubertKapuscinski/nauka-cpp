20754dad771c3763be16edca77aca74e97f7c591
hubertk <hubert.kapuscinski27@wp.pl>
2022-01-19 14:08:08 +0100

    added more files

---
+++ Filestream.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    cout << "What do u want to write into file?" << endl;
+    string text;
+    //Read input and write to variable
+    cin >> text;
+
+    //Create a variable File
+    fstream file;
+    //Open a file for a write
+    file.open("file.txt", ios::out);
+    //Write to a file
+    file << text;
+    //Close file
+    file.close();
+
+    //Read a file
+    char ch;
+	while (1) {
+		file >> ch;
+		if (file.eof()) break;
+		cout << ch;
+	}
+    
+}
