b32c07e2fc29416de94dde1ba286e30ef690af72
hubertk <hubert.kapuscinski27@wp.pl>
2022-01-20 10:14:02 +0100

    Simle math equations

--- Math.cpp
+++ Math.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-#include <math.h>
+#include <cmath>
 using namespace std;
 
 int main() {
-    
+    cout << "Square root of 64 is " << sqrt(64) << endl;
+    cout << "2.6 rounded is " << round(2.6) << endl;
+    cout << "Logarithm of 2 is " << log(2) << endl;
 }
