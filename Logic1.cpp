400ad545548fe689d0214c2bec938e8e6f4968fa
hubertk <hubert.kapuscinski27@wp.pl>
2022-01-21 10:46:37 +0100

    Simple logic task

--- Logic1.cpp
+++ Logic1.cpp
@@ -2,19 +2,13 @@
 using namespace std;
 
 string needs_water(int days, bool is_succulent) {
-    
-  if (!is_succulent && days > 3)
-    return "Time to water the plant.";
-
-  else if (is_succulent && days <= 12)
-    return "Don't water the plant!";
-
-  else if (is_succulent && days >= 13)
-    return "Go ahead and give the plant a little water.";
-
+  if (!is_succulent && days > 3) return "Time to water the plant.";
+  else if (is_succulent && days <= 12) return "Don't water the plant!";
+  else if (is_succulent && days >= 13) return "Go ahead and give the plant a little water.";
   else return "Don't water the plant!";
 }
 
 int main() {
-    cout << needs_water(10, false) << "\n";
+  cout << needs_water(10, false) << "\n";
+  return 0;
 }
