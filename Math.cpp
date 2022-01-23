80222fbb9375b9d6570e5a1f97bd4bcf0d3ba6c6
hubertk <hubert.kapuscinski27@wp.pl>
2022-01-23 21:18:56 +0100

    Simple math tasks template

--- Math.cpp
+++ Math.cpp
@@ -6,4 +6,5 @@ int main() {
     cout << "Square root of 64 is " << sqrt(64) << endl;
     cout << "2.6 rounded is " << round(2.6) << endl;
     cout << "Logarithm of 2 is " << log(2) << endl;
+    return 0;
 }
