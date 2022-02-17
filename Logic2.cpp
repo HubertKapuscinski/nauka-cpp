35c9dc2fb4a8971be8562b964f7581299fb56e99
hubertk <hubert.kapuscinski27@wp.pl>
2022-02-17 16:55:33 +0100

    commit didnt work lol

--- Logic2.cpp
+++ Logic2.cpp
@@ -3,17 +3,13 @@ using namespace std;
 
 bool is_palindrome(string text) {
   string reversed_text = "";
-  
-  for (int i = text.size() - 1; i >= 0; i--)
-    reversed_text += text[i];
-  
+  for (int i = text.size() - 1; i >= 0; i--) reversed_text += text[i];
   if (reversed_text == text) return true;
-  
   return false;
 }
 
 int main() {
-  std::cout << is_palindrome("madam") << "\n";
-  std::cout << is_palindrome("ada") << "\n";
-  std::cout << is_palindrome("lovelace") << "\n";
+  cout << is_palindrome("madam") << endl;
+  cout << is_palindrome("ada") << endl;
+  cout << is_palindrome("lovelace") << endl;
 }
