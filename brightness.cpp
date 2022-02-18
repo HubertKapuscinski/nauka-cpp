a54b51b37d3c38df4fad97b12e76b09ae8334998
hubertk <hubert.kapuscinski27@wp.pl>
2022-02-18 13:01:58 +0100

    Change brightness on linux. Added support for cfg file.

--- brightness.cpp
+++ brightness.cpp
@@ -1,19 +1,58 @@
 #include <iostream>
 #include <fstream>
+#include <unistd.h>
 
 using namespace std;
 
 //Variables
 string path = "/sys/class/backlight/amdgpu_bl0/brightness";
+string cfgPath = "/home/";
+string userPath;
 string source;
 string brightness;
 string decision;
+string username = getlogin();
 fstream brFile;
+fstream cfgFile;
 int value;
 
+void error() {
+    cout << endl << "Error occured while trying to open cfg file." << endl;
+    exit(0);
+}
+
 int main() {
+    cfgPath += username;
+    cfgPath += "/.config/brightness/brightness.conf";
+
+    cfgFile.open(cfgPath);
+    if (cfgFile.good() == false) {
+        cout << "There was no config file found. Creating." << endl
+             << "Write your path to brightness configuration file." << endl 
+             << "(Default = " << path << ")" << endl;
+        cin >> userPath;
+        
+        system("mkdir ~/.config/brightness");
+        system("touch ~/.config/brightness/brightness.conf");
+
+        cfgFile.open(cfgPath);
+        if (cfgFile.good() == false) error();
+        else {
+            cfgFile << userPath;
+            cfgFile.close();
+            cout << "Config file created succesfuly." << endl;
+        }
+    }
+
     cout << "Current brightness value:";
     
+    cfgFile.open(cfgPath, ios::in | ios::out);
+    if (cfgFile.is_open()) {
+        getline(cfgFile, path);
+        brFile.close();
+    }
+    else error();
+
     brFile.open(path, ios::in | ios::out);
     if (brFile.good() == true) {
         getline(brFile, brightness);
@@ -34,6 +73,8 @@ int main() {
                     remove("/sys/class/backlight/amdgpu_bl0c");
                     brFile.open(path);
                     brFile << value;
+                    brFile.close();
+                    brFile.open(path);
                     getline(brFile, brightness);
                     brFile.close();
                     
