efb4d3ef2d749f3976fc2ebd94f4ddae40b66518
Hubert Kapuściński <hubert.kapuscinski27@wp.pl>
2022-02-19 14:29:45 +0100

    Added support for cli arguments

--- brightness.cpp
+++ brightness.cpp
@@ -1,30 +1,78 @@
 #include <iostream>
 #include <fstream>
 #include <unistd.h>
+#include <string>
 
 using namespace std;
 
 //Variables
 string path = "/sys/class/backlight/amdgpu_bl0/brightness";
 string cfgPath = "/home/";
+string username = getlogin();
 string userPath;
 string source;
 string brightness;
 string decision;
-string username = getlogin();
 fstream brFile;
 fstream cfgFile;
 int value;
+int argVal;
 
-void error() {
-    cout << endl << "Error occured while trying to open cfg file." << endl;
+void error(int errVal) {
+    switch (errVal) {
+        case 1:
+            cout << "Error occured while trying to open cfg file." << endl;
+            break;
+        case 2:
+            cout << "Error while reading arguments." << endl 
+                 << "Max arguments is 1 - brightness value." << endl;
+            break;
+        case 3:
+            cout << "Error occured while opening brightness file." << endl;
+            break;
+        case 4:
+            cout << "Error. Max value = 255 and min value = 0." << endl;
+            break;
+        default:
+            cout << "Unhandled exception." << endl;
+            break;
+    }
     exit(0);
 }
 
-int main() {
-    cfgPath += username;
-    cfgPath += "/.config/brightness/brightness.conf";
+void openCfgFile() {
+    cfgFile.open(cfgPath, ios::in | ios::out);
+    if (cfgFile.is_open()) {
+        getline(cfgFile, path);
+        cfgFile.close();
+    }
+    else error(1);
+}
 
+int main(int argc, char **argValue) {
+    //Complete
+    cfgPath += username;
+    cfgPath += "/.config/brightness/brightness.cfg";
+    
+    //Run with args
+    if (argc == 2) {
+        sscanf (argValue[1],"%d",&argVal);
+        if (argVal > 255 || argVal < 0) error(4);
+
+        openCfgFile();
+        remove("/sys/class/backlight/amdgpu_bl0/brightness");
+        brFile.open(path, ios::in | ios::out);
+        if (brFile.is_open()) {
+            brFile << argVal;
+            brFile.close();
+        }
+        else error(3);
+
+        exit(0);
+    }
+    else if (argc > 2) error(2);
+
+    //Creating cfg file
     cfgFile.open(cfgPath);
     if (cfgFile.good() == false) {
         cout << "There was no config file found. Creating." << endl
@@ -32,11 +80,13 @@ int main() {
              << "(Default = " << path << ")" << endl;
         cin >> userPath;
         
-        system("mkdir ~/.config/brightness");
-        system("touch ~/.config/brightness/brightness.conf");
+        system("mkdir ~/.config/brightness/");
+        system("touch ~/.config/brightness/brightness.cfg");
+
+        cout << cfgPath << endl;
 
         cfgFile.open(cfgPath);
-        if (cfgFile.good() == false) error();
+        if (cfgFile.is_open() == false) error(1);
         else {
             cfgFile << userPath;
             cfgFile.close();
@@ -46,12 +96,7 @@ int main() {
 
     cout << "Current brightness value:";
     
-    cfgFile.open(cfgPath, ios::in | ios::out);
-    if (cfgFile.is_open()) {
-        getline(cfgFile, path);
-        brFile.close();
-    }
-    else error();
+    openCfgFile();
 
     brFile.open(path, ios::in | ios::out);
     if (brFile.good() == true) {
@@ -70,7 +115,7 @@ int main() {
             
                 if (value > 255 || value < 0) cout << "Error, wrong value." << endl;
                 else {
-                    remove("/sys/class/backlight/amdgpu_bl0c");
+                    remove("/sys/class/backlight/amdgpu_bl0/brightness");
                     brFile.open(path);
                     brFile << value;
                     brFile.close();
